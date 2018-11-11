4a8f2e66f13fab4753e3f72c0b6dd6a64e4ba5b1
KenjiTakahashi <wozniakk@gmail.com>
2018-11-12 00:26:15 +0100

    Overly-zealous locking of the DB
    Better safe than sorry, ey?
    Could possibly be optimized at later stage.

--- db/memdb.go
+++ db/memdb.go
@@ -1,12 +1,15 @@
 package db
 
 import (
+	"sync"
 	"time"
+
+	"go.uber.org/atomic"
 )
 
 type customerState struct {
 	reserved time.Time
-	charged  bool
+	charged  *atomic.Bool
 }
 
 type MemTicketsDB struct {
@@ -16,6 +19,7 @@ type MemTicketsDB struct {
 	// probably sit in the testing module and a "real"
 	// DB implementation will sit here instead.
 	Customers map[string]*customerState
+	lock      sync.RWMutex
 }
 
 func NewMemTicketsDB(number int) *MemTicketsDB {
@@ -27,55 +31,72 @@ func NewMemTicketsDB(number int) *MemTicketsDB {
 
 func (m *MemTicketsDB) Remaining() (int, error) {
 	now := time.Now().UTC()
+	m.lock.Lock()
 	for _, cs := range m.Customers {
-		if !cs.charged && now.Sub(cs.reserved).Minutes() > 5 {
+		if !cs.charged.Load() && now.Sub(cs.reserved).Minutes() > 5 {
 			m.remaining++
 		}
 	}
+	m.lock.Unlock()
 	return m.remaining, nil
 }
 
 func (m *MemTicketsDB) Reserve(fullname string) error {
+	m.lock.RLock()
 	customer, exists := m.Customers[fullname]
+	m.lock.RUnlock()
 	if !exists {
+		m.lock.Lock()
+
 		if m.remaining <= 0 {
+			m.lock.Unlock()
 			return ErrNoTicketsRemaining
 		}
-		m.remaining-- // TODO: Make this atomic
+		m.remaining--
 		m.Customers[fullname] = &customerState{
 			reserved: time.Now().UTC(),
-			charged:  false,
+			charged:  atomic.NewBool(false),
 		}
+
+		m.lock.Unlock()
 		return nil
+	}
+	if customer.charged.Load() {
+		return ErrAlreadyCharged
 	}
 	customer.reserved = time.Now().UTC()
 	return nil
 }
 
 func (m *MemTicketsDB) Charge(fullname string) error {
+	m.lock.RLock()
 	customer, exists := m.Customers[fullname]
+	m.lock.RUnlock()
 	if !exists {
 		return ErrNoReservation
 	}
-	if customer.charged {
-		return ErrAlreadyCharged
+	if customer.charged.CAS(false, true) {
+		return nil
 	}
-	customer.charged = true
-	return nil
+	return ErrAlreadyCharged
 }
 
 func (m *MemTicketsDB) Guests() ([]string, error) {
 	names := []string{}
+	m.lock.RLock()
 	for fullname, cs := range m.Customers {
-		if cs.charged {
+		if cs.charged.Load() {
 			names = append(names, fullname)
 		}
 	}
+	m.lock.RUnlock()
 	return names, nil
 }
 
 func (m *MemTicketsDB) Reset() error {
+	m.lock.Lock()
 	m.remaining += len(m.Customers)
 	m.Customers = map[string]*customerState{}
+	m.lock.Unlock()
 	return nil
 }
